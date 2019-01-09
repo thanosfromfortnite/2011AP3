57bdb1924a66da57b521bc574e0eb2923450777d
BT_1N3_27 <d>
2019-01-09 11:10:08 -0500

    d

--- src/FuelDepot.java
+++ src/FuelDepot.java
@@ -2,13 +2,31 @@ import java.util.List;
 
 public class FuelDepot {
 
+    /* Fields */
     private FuelRobot filler;
     private List<FuelTank> tanks;
 
-    public FuelDepot() {
-
+    /* Constructors */
+    public FuelDepot(boolean robotFacingRight, int robotIndex, int[] fuel) {
+        filler = new CoolFuelRobot(robotIndex, robotFacingRight);
+        for (int i = 0; i < fuel.length; i ++) {
+            tanks.add(new CoolFuelTank(fuel[i]));
+        }
+    }
+    public FuelDepot(boolean robotFacingRight, int robotIndex, List<Integer> fuel) {
+        filler = new CoolFuelRobot(robotIndex, robotFacingRight);
+        for (int i = 0; i < fuel.size(); i ++) {
+            tanks.add(new CoolFuelTank(fuel.get(i)));
+        }
     }
 
+    /* Methods */
+
+    /**
+     * @param threshold The threshold number.
+     * @return The index of the lowest leveled fuel tank under the threshold. If there are none, it will return the
+     * current index.
+     */
     public int nextTankToFill(int threshold) {
         int index = filler.getCurrentIndex(), lowest = threshold;
         for (int i = 0; i < tanks.size(); i ++) {
@@ -19,6 +37,10 @@ public class FuelDepot {
         return index;
     }
 
+    /**
+     * Move to a specified index.
+     * @param locIndex The space to move to. The robot will change direction for this if needed.
+     */
     public void moveToLocation(int locIndex) {
         boolean positive = locIndex >= filler.getCurrentIndex();
         if (positive != filler.isFacingRight()) {
